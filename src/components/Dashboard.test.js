01ad56f46c8b8c4e2f70e36cf8aa5d19a88942b8
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-08-08 14:12:29 -0500

    created component file structure

---
+++ src/components/Dashboard.test.js
@@ -0,0 +1,17 @@
+import { } from './Dashboard';
+import { italic } from 'ansi-colors';
+
+
+
+describe('Dashboard.js', () => {
+    describe('strike()', () =>{
+        it('should add the points for a strike to the current total',() =>{
+            //expects go here
+        });
+    });
+
+    describe('ball()')
+})
+// - provide a button that the person in charge can press every time there is a `strike`, `ball`, `foul` or `hit`.
+// - changes recorded on this component should update the information shown by the `Display` component.
+
