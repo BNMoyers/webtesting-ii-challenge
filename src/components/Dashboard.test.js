ace1ee08ebb954e9542550b3e84f3ec0dc6a6fe7
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-08-08 14:21:38 -0500

    created test logic for Dashboard

--- src/components/Dashboard.test.js
+++ src/components/Dashboard.test.js
@@ -1,16 +1,33 @@
-import { } from './Dashboard';
-import { italic } from 'ansi-colors';
+import { strike, ball, foul, hit } from './Dashboard';
+
 
 
 
 describe('Dashboard.js', () => {
     describe('strike()', () =>{
-        it('should add the points for a strike to the current total',() =>{
+        it('should add 1 to strike total on button press, up to 3, then reset',() =>{
             //expects go here
         });
     });
 
-    describe('ball()')
+    describe('ball()', () =>{
+        it('should add 1 to ball total on button press, up to 4, then reset', () =>{
+
+        }); 
+    });
+
+    describe('foul()', () =>{
+        it('should add 1 to strike total on button press, unless strike total = 2', () =>{
+
+        });
+    });
+
+    describe('hit()', () =>{
+        it('should reset the balls and strikes to zero on button press', () =>{
+
+        });
+    });
+
 })
 // - provide a button that the person in charge can press every time there is a `strike`, `ball`, `foul` or `hit`.
 // - changes recorded on this component should update the information shown by the `Display` component.
